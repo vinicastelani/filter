cc6b4acb97f4a05a1fc7e3afaa8590aa9c0dcbb7
Vinicius Castelani dos Santos <viniciuscastelanidossantos@Viniciuss-MBP.lan>
2020-09-30 13:54:31 -0300

    INITIAL COMMIT

---
+++ js/script.js
@@ -0,0 +1,345 @@
+var arr = [
+    {
+        filme: "Esposa de Mentirinha",
+        text: {
+            desc: "Infeliz em sua tentativa de casamento, Danny passa a vivenciar relacionamentos sem compromisso para driblar a carência. Assim, ele toca sua vida como cirurgião plástico bem sucedido, tendo sua melhor amiga Katherine, mãe solteira de um casal, como fiel escudeira. Quando conhece a jovem Palmer e a paixão toma conta de ambos, ele inventa, para tentar conquistá-la, que é marido de Katherine, pai das crianças e que vai se separar. Assim começa uma verdadeira aventura amorosa recheada de confusões.",
+            ano: new Date("4 Mar 2011")
+        },
+        img: {
+            path: "./images/filme1.jpeg",
+            alt: "Esposa de Mentirinha"
+        },
+        keywords: "Romance"
+    },
+    {
+        filme: "Com Amor, Simon",
+        text: {
+            desc: "Aos 17 anos, Simon Spier aparenta levar uma vida comum, mas sofre por esconder um grande segredo: não revelou ser gay para sua família e amigos. E tudo fica mais complicado quando ele se apaixona por um dos colegas de classe, anônimo, na internet.",
+            ano: new Date("22 Mar 2018")
+        },
+        img: {
+            path: "./images/filme2.jpg",
+            alt: "Com Amor, Simon"
+        },
+        keywords: "Romance"
+    },
+    {
+        filme: "Professora Sem Classe",
+        text: {
+            desc: "Para a maioria, o ensino é uma profissão honrosa, exceto para Elizabeth. A desbocada e embriagada professora mal pode esperar para se casar com um homem rico e largar o emprego, mas assim que seu namorado a deixa, ela precisa repensar sobre seus planos. Quando Scott Delacorte, um professor substituto bonito e rico chega à escola, Elizabeth encontra nele uma saída para sua situação, mas terá de enfrentar a forte concorrência de Amy, uma colega popular e alegre.",
+            ano: new Date("19 Aug 2011")
+        },
+        img: {
+            path: "./images/filme3.jpeg",
+            alt: "Professora Sem Classe"
+        },
+        keywords: "Comédia"
+    },
+    {
+        filme: "Forrest Gump - O Contador de Histórias",
+        text: {
+            desc: "Mesmo com o raciocínio lento, Forrest Gump nunca se sentiu desfavorecido. Graças ao apoio da mãe, ele teve uma vida normal. Seja no campo de futebol como um astro do esporte, lutando no Vietnã ou como capitão de um barco de camarão, Forrest inspira a todos com seu otimismo infantil. Mas a pessoa com quem Forrest mais se preocupa pode ser a mais difícil de salvar: seu amor de infância, a doce, mas perturbada Jenny.",
+            ano: new Date("7 Dec 1994")
+        },
+        img: {
+            path: "./images/filme5.jpeg",
+            alt: "Forrest Gump - O Contador de Histórias"
+        },
+        keywords: "Drama"
+    },
+    {
+        filme: "Divertida Mente",
+        text: {
+            desc: "Com a mudança para uma nova cidade, as emoções de Riley, que tem apenas 11 anos de idade, ficam extremamente agitadas. Uma confusão na sala de controle do seu cérebro deixa a Alegria e a Tristeza de fora, afetando a vida de Riley radicalmente.",
+            ano: new Date("19 Jun 2015")
+        },
+        img: {
+            path: "./images/filme6.jpeg",
+            alt: "Divertida Mente"
+        },
+        keywords: "Animação"
+    },
+    {
+        filme: "Tempestade: Planeta em Fúria",
+        text: {
+            desc: "A ocorrência cada vez mais frequente de eventos climáticos capazes de ameaçar a existência da humanidade faz com que seja criada uma extensa rede de satélites, ao redor de todo o planeta, de forma a controlar o próprio clima. Apelidado de Dutch Boy, este sistema construído a partir da cooperação de 17 países é coordenado pelo engenheiro Jake Lawson. Após anos de dedicação, ele é afastado da função devido a questões políticas e, em seu lugar, é nomeado seu irmão caçula, Max.",
+            ano: new Date("19 Oct 2017")
+        },
+        img: {
+            path: "./images/filme7.jpeg",
+            alt: "Tempestade: Planeta em Fúria"
+        },
+        keywords: "Ação"
+    },
+    {
+        filme: "The Bye Bye Man",
+        text: {
+            desc: "Três estudantes universitários decidem sair para acampar no tranquilo estado do Wisconsin, nos Estados Unidos. Mas os planos de terem alguns dias de diversão e descanso são interrompidos quando suas vidas se cruzam com o temido 'Bye Bye Man', uma criatura mística e sobrenatural que acaba perseguindo-os. Agora eles vão ter que lutar para sobreviver, contra este que é a raiz de todos os atos maus do homem.",
+            ano: new Date("9 Feb 2017")
+        },
+        img: {
+            path: "./images/filme8.jpeg",
+            alt: "The Bye Bye Man"
+        },
+        keywords: "Terror"
+    },
+    {
+        filme: "Esposa de Mentirinha",
+        text: {
+            desc: "Infeliz em sua tentativa de casamento, Danny passa a vivenciar relacionamentos sem compromisso para driblar a carência. Assim, ele toca sua vida como cirurgião plástico bem sucedido, tendo sua melhor amiga Katherine, mãe solteira de um casal, como fiel escudeira. Quando conhece a jovem Palmer e a paixão toma conta de ambos, ele inventa, para tentar conquistá-la, que é marido de Katherine, pai das crianças e que vai se separar. Assim começa uma verdadeira aventura amorosa recheada de confusões.",
+            ano: new Date("4 Mar 2011")
+        },
+        img: {
+            path: "./images/filme1.jpeg",
+            alt: "Esposa de Mentirinha"
+        },
+        keywords: "Romance"
+    },
+    {
+        filme: "Com Amor, Simon",
+        text: {
+            desc: "Aos 17 anos, Simon Spier aparenta levar uma vida comum, mas sofre por esconder um grande segredo: não revelou ser gay para sua família e amigos. E tudo fica mais complicado quando ele se apaixona por um dos colegas de classe, anônimo, na internet.",
+            ano: new Date("22 Mar 2018")
+        },
+        img: {
+            path: "./images/filme2.jpg",
+            alt: "Com Amor, Simon"
+        },
+        keywords: "Romance"
+    },
+    {
+        filme: "Professora Sem Classe",
+        text: {
+            desc: "Para a maioria, o ensino é uma profissão honrosa, exceto para Elizabeth. A desbocada e embriagada professora mal pode esperar para se casar com um homem rico e largar o emprego, mas assim que seu namorado a deixa, ela precisa repensar sobre seus planos. Quando Scott Delacorte, um professor substituto bonito e rico chega à escola, Elizabeth encontra nele uma saída para sua situação, mas terá de enfrentar a forte concorrência de Amy, uma colega popular e alegre.",
+            ano: new Date("19 Aug 2011")
+        },
+        img: {
+            path: "./images/filme3.jpeg",
+            alt: "Professora Sem Classe"
+        },
+        keywords: "Comédia"
+    },
+    {
+        filme: "Forrest Gump - O Contador de Histórias",
+        text: {
+            desc: "Mesmo com o raciocínio lento, Forrest Gump nunca se sentiu desfavorecido. Graças ao apoio da mãe, ele teve uma vida normal. Seja no campo de futebol como um astro do esporte, lutando no Vietnã ou como capitão de um barco de camarão, Forrest inspira a todos com seu otimismo infantil. Mas a pessoa com quem Forrest mais se preocupa pode ser a mais difícil de salvar: seu amor de infância, a doce, mas perturbada Jenny.",
+            ano: new Date("7 Dec 1994")
+        },
+        img: {
+            path: "./images/filme5.jpeg",
+            alt: "Forrest Gump - O Contador de Histórias"
+        },
+        keywords: "Drama"
+    },
+    {
+        filme: "Divertida Mente",
+        text: {
+            desc: "Com a mudança para uma nova cidade, as emoções de Riley, que tem apenas 11 anos de idade, ficam extremamente agitadas. Uma confusão na sala de controle do seu cérebro deixa a Alegria e a Tristeza de fora, afetando a vida de Riley radicalmente.",
+            ano: new Date("19 Jun 2015")
+        },
+        img: {
+            path: "./images/filme6.jpeg",
+            alt: "Divertida Mente"
+        },
+        keywords: "Animação"
+    },
+    {
+        filme: "Tempestade: Planeta em Fúria",
+        text: {
+            desc: "A ocorrência cada vez mais frequente de eventos climáticos capazes de ameaçar a existência da humanidade faz com que seja criada uma extensa rede de satélites, ao redor de todo o planeta, de forma a controlar o próprio clima. Apelidado de Dutch Boy, este sistema construído a partir da cooperação de 17 países é coordenado pelo engenheiro Jake Lawson. Após anos de dedicação, ele é afastado da função devido a questões políticas e, em seu lugar, é nomeado seu irmão caçula, Max.",
+            ano: new Date("19 Oct 2017")
+        },
+        img: {
+            path: "./images/filme7.jpeg",
+            alt: "Tempestade: Planeta em Fúria"
+        },
+        keywords: "Ação"
+    },
+    {
+        filme: "The Bye Bye Man",
+        text: {
+            desc: "Três estudantes universitários decidem sair para acampar no tranquilo estado do Wisconsin, nos Estados Unidos. Mas os planos de terem alguns dias de diversão e descanso são interrompidos quando suas vidas se cruzam com o temido 'Bye Bye Man', uma criatura mística e sobrenatural que acaba perseguindo-os. Agora eles vão ter que lutar para sobreviver, contra este que é a raiz de todos os atos maus do homem.",
+            ano: new Date("9 Feb 2017")
+        },
+        img: {
+            path: "./images/filme8.jpeg",
+            alt: "The Bye Bye Man"
+        },
+        keywords: "Terror"
+    },
+]
+filteredList = []
+var organizedArr = []
+//Filtra o array principal (arr) a partir no input:text
+//E executa a funcao loader passando o array filtrado
+function filterByInput(arr, string) {
+    var holder = []
+    //Desativa os input:checkbox 
+    clearAllCheckboxes()
+    //Remove todos os cards criados a cada input:text
+    //Para evitar criar cards repetidos 
+    clearDOM('loader')
+    //Se o input estiver vazio, retorna o array original
+    if (string.length == 0) {
+        //Retorna a funçao que cria os cards, passando array
+        //E div parent
+        return loader(separateArr(arr), '#loader')
+    }
+    //Busca no array original (arr) os objetos que possuem os valores
+    //Digitados no input iguais aos atributos el.text.desc e el.filme
+    else {
+        holder = arr.filter((el) => {
+            return el.text.desc.toLowerCase().indexOf(string.toLowerCase()) > -1 || el.filme.toLowerCase().indexOf(string.toLowerCase()) > -1 || el.keywords.toLowerCase().indexOf(string.toLowerCase()) > -1
+        })
+    }
+    //Retorna a funçao que cria os cards, passando array
+    //E div parent
+    return loader(separateArr(holder), '#loader')
+}
+
+//Filtra o array principal (arr) a partir no input:checkbox
+//E executa a funcao loader passando o array filtrado
+function filterByCheckbox(arr, string, status) {
+    //Apaga o value de input:text
+    document.getElementById("input").value = ""
+    //Remove todos os cards criados a cada input:text
+    //Para evitar criar cards repetidos 
+    clearDOM('loader')
+    //Busca no array original (arr) os objetos que possuem os valores
+    //Recebidos no input:checkbox no atributo el.keywords
+    arr.map((el) => {
+        //Se status, valida a segunda condicao e pusha para filteredList 
+        if (status) {
+            if (el.keywords == string) {
+                filteredList.push(el)
+            }
+        }
+        //Se !status, verifica quais cards devem ser removidos de filteredList 
+        else {
+            var toBeRemoved = filteredList.filter(e => e.keywords === string);
+            toBeRemoved.forEach(f => filteredList.splice(filteredList.findIndex(e => e.keywords === string), 1));
+        }
+    })
+    return validateCheckbox() ? loader(separateArr(filteredList), '#loader') : loader(separateArr(arr), '#loader')
+}
+
+//Cria os cards, recebendo array e div parent 
+function loader(arr, target) {
+    arr.forEach((slice, index) => {
+        var html = `<div class="slice" data-slice="${index}">`
+        slice.forEach((item) => {
+            html += `
+               <div>
+                    <img src="${item.img.path}" alt="${item.img.alt}">
+                    <h1 class="header">${item.filme}</h1>
+                    <p class="desc">${item.text.desc}</p>
+                    <p class="date">${item.text.ano.toLocaleDateString('pt-BR')}</p>
+                </div>
+                `
+        })
+        html += `</div>`
+        $(target).append(html)
+    })
+    buildNavigation(arr)
+}
+
+//Cria os input:checkbox de acordo com as keywords dos cards no array principal
+function buildList(arr, target) {
+    //Inicia um objeto  
+    var counts = {};
+    arr.forEach((el) => {
+        //Atribui ao objeto counts a string no el.keywords e quantas vezes ela se repete
+        counts[el.keywords] = (counts[el.keywords] || 0) + 1;
+    });
+    //Itera o objeto, criando a lista de inputs 
+    for (var element in counts) {
+        var html = `
+            <input type="checkbox" id="${element}" name="${element}" value="${element}" onclick="filterByCheckbox(arr, '${element}',this.checked)">
+            <label for="${element}">${element + ' (' + counts[element] + ')'}</label><br>
+        `
+        $(target).append(html)
+    }
+}
+
+
+//Itera os input:checkbox para validar se há pelo menos um
+//Checked 
+function validateCheckbox() {
+    var bool = false
+    var div = $(" :checkbox")
+    for (var input of div) {
+        input.checked == true ? bool = true : bool
+    }
+    return bool
+}
+//Desabilita os input:checkbox 
+function clearAllCheckboxes() {
+    var div = $(" :checkbox")
+    for (var i = 0; i < div.length; i++) {
+        div[i].checked = false
+    }
+    clearDOM('loader')
+    filteredList = []
+    return loader(separateArr(arr), '#loader')
+}
+//Apaga as divs criadas para evitar repetição de cards 
+function clearDOM(target) {
+    var node = document.getElementById(target);
+    node.querySelectorAll('*').forEach(n => n.remove());
+}
+
+
+//Retorna o array dividido a cada 4 elementos
+function separateArr(arr) {
+    var array = arr.map(x => x)
+    organizedArr = [],
+        size = 4;
+    while (array.length > 0) organizedArr.push(array.splice(0, size));
+    return organizedArr;
+}
+
+//Build the navigation numbers
+function buildNavigation(arr) {
+    //Limpa a navegacao para recria-la sem repeticoes 
+    clearDOM('navigation')
+    //Inicia a página com o primeiro slice a mostra 
+    changeTab(0)
+    arr.forEach((item, index) => {
+        var html = `
+            <p class="nav__slice" data-slice="${index}" onclick="changeTab(this.getAttribute('data-slice'))">${index}</p>
+        `
+        $("#navigation").append(html);
+    })
+}
+
+//Alterna entre os slices 
+function changeTab(int) {
+    var html = document.getElementsByClassName("slice")
+    for (slice of html) {
+        slice.getAttribute("data-slice") == int ? slice.style = "display:flex" : slice.style = "display:none"
+    }
+    changeTabOnArrows()
+}
+
+function changeTabOnArrows() {
+    var html = document.getElementsByClassName("slice")
+    var forward = document.getElementById("arr_forward")
+    var backward = document.getElementById("arr_backward")
+    for (slice of html) {
+        var isShowing
+        // var isShowing = slice.style.display == "flex" ? slice.getAttribute('data-slice') : false
+        if (slice.style.display == "flex") {
+            isShowing = slice.getAttribute('data-slice')
+        }
+    }
+    if (isShowing == 0) {
+        backward.setAttribute("data-slice", parseInt(isShowing))
+        forward.setAttribute("data-slice", parseInt(isShowing) + 1)
+    } else if (isShowing > 0 && isShowing < organizedArr.length - 1) {
+        backward.setAttribute("data-slice", parseInt(isShowing) - 1)
+        forward.setAttribute("data-slice", parseInt(isShowing) + 1)
+    } else {
+        backward.setAttribute("data-slice", parseInt(isShowing) - 1)
+        forward.setAttribute("data-slice", parseInt(isShowing))
+    }
+}
